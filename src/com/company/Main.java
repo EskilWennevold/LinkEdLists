4fa05da52c267a7aaf97d64829a0e7ccc6403ab2
eskilwennevold <eskilwennevold@hotmail.com>
2021-02-11 17:20:10 +0100

    Den Første færdige version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,19 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         MyLinkedList list = new MyLinkedList();
         Scanner scan = new Scanner(System.in);
+        loadFile(list);
         mainMenu(scan, list);
+        list.toFile();
     }
+
     public static void mainMenu(Scanner scan, MyLinkedList list){
 
         int svar = 1;
@@ -37,29 +42,44 @@ public class Main {
                 list.getPlayers();
                 break;
             case 3:
-
-                searchPlayers();
+                searchPlayers(list, scan);
                 break;
             case 4:
                 deletePlayer(scan, list);
                 break;
             case 5:
-                search1Team();
+                search1Team(list);
                 break;
             case 6:
-                search2Team();
+                search2Team(list);
                 break;
             case 7:
-                searchInterval();
+                searchInterval(scan, list);
                 break;
 
             }
         }
     }
-
-    private static void searchInterval() {
+    private static void searchInterval(Scanner scan, MyLinkedList list) {
+        System.out.println("Type in the minimal age in the interval you want to search for: ");
+        int minAge = scan.nextInt();
+        System.out.println("Type int tha maximum age in the interval you want to search for: ");
+        int maxAge = scan.nextInt();
+        list.getInterval(minAge, maxAge);
     }
-
+    public static void loadFile(MyLinkedList list) throws FileNotFoundException {
+        Scanner load = new Scanner(new File("players.txt"));
+        while (load.hasNextLine()) {
+            String line = load.nextLine();
+            Scanner lineScan = new Scanner(line);
+            String fname = lineScan.next();
+            String lname = lineScan.next();
+            int age = lineScan.nextInt();
+            int team = lineScan.nextInt();
+            Player p1 = new Player(fname, lname, age, team);
+            list.add(p1);
+        }
+    }
     public static void createPlayer(Scanner scan, MyLinkedList list) {
         System.out.print("Type the players first name: ");
         String f = scan.next();
@@ -73,9 +93,10 @@ public class Main {
         Player p = new Player(f,l,a,t);
         list.add(p);
     }
-
-    public static void searchPlayers(){
-
+    public static void searchPlayers(MyLinkedList list, Scanner scan){
+        System.out.println("What is the first name of the player you want to see?");
+        String f = scan.next();
+        list.getPlayer(f);
     }
     public static void deletePlayer(Scanner scan, MyLinkedList list){
         System.out.println("Type in the first name of the player you want to delete:");
@@ -83,11 +104,12 @@ public class Main {
         list.remove(f);
 
     }
-    public static void search1Team(){
+    public static void search1Team(MyLinkedList list){
+        list.getTeam1();
+    }
+    public static void search2Team(MyLinkedList list){
+        list.getTeam2();
 
     }
-    public static void search2Team(){
 
-
-    }
 }
--- src/com/company/MyLinkedList.java
+++ src/com/company/MyLinkedList.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class MyLinkedList {
 
     private ListNode front;
@@ -7,6 +11,7 @@ public class MyLinkedList {
     public MyLinkedList(){
         front = null;
     }
+
     public void getPlayers(){
         ListNode current = front;
         System.out.println(front.getData().getFname() + " " + front.getData().getLname());
@@ -16,7 +21,20 @@ public class MyLinkedList {
         }
     }
 
-
+    public void getPlayer(String f){
+        if(front != null){
+            if(front.getData().getFname().equalsIgnoreCase(f)){
+                System.out.println(front);
+            }
+            ListNode current = front;
+            while(current.next != null){
+                if(current.next.getData().getFname().equalsIgnoreCase(f)){
+                    System.out.println(current.next);
+                }
+                current = current.next;
+            }
+        }
+    }
     public void add(Player player){
         if(front == null){
             front = new ListNode(player);
@@ -28,6 +46,7 @@ public class MyLinkedList {
             current.setNext(new ListNode(player));
         }
     }
+
     public void remove(String name){
         if(front != null) {
             ListNode current = front;
@@ -58,9 +77,73 @@ public class MyLinkedList {
             result += "]";
             return result;
         }
-
     }
 
+    public void getInterval(int minAge, int maxAge) {
+        if(front.getData().getAge() >= minAge && front.getData().getAge() <= maxAge){
+            System.out.println(front.getData().getFname() + " " + front.getData().getLname() + " " + front.getData().getAge());
+        }
+        ListNode current = front;
+        while(current.next != null){
+            if(current.next.getData().getAge() >= minAge && current.next.getData().getAge() <= maxAge){
+                System.out.println(current.next.getData().getFname() + " " + current.next.getData().getLname() + " " + current.next.getData().getAge());
+            }
+            current = current.next;
+        }
+    }
+    public void toFile() throws FileNotFoundException {
+        if(front != null) {
+        PrintStream p1 = new PrintStream(new File("players.txt"));
+            String fName = front.getData().getFname();
+            String lName = front.getData().getLname();
+            int age = front.getData().getAge();
+            int team = front.getData().getTeam();
+            String line = fName + " " + lName + " " + age + " " + team;
+            p1.println(line);
+            ListNode current = front;
+            while(current.next != null){
+                fName = current.next.getData().getFname();
+                lName = current.next.getData().getLname();
+                age = current.next.getData().getAge();
+                team = current.next.getData().getTeam();
+                String line1 = fName + " " + lName + " " + age + " " + team;
+                p1.println(line1);
+                current = current.next;
+            }
+        }
+    }
+    public void getTeam1(){
+        if(front == null){
+            System.out.println("This list is empty");
+        } else {
+            if(front.getData().getTeam() == 1){
+                System.out.println(front.getData().getFname() + " " + front.getData().getLname());
+            }
+            ListNode current = front;
+            while(current.next != null){
+                if(current.next.getData().getTeam() == 1){
+                    System.out.println(current.next.getData().getFname() + " " + current.next.getData().getLname());
+                }
+                current = current.next;
+            }
+        }
 
+    }
+    public void getTeam2(){
+        if(front == null){
+            System.out.println("This list is empty");
+        } else {
+            if(front.getData().getTeam() == 2){
+                System.out.println(front.getData().getFname() + " " + front.getData().getLname());
+            }
+            ListNode current = front;
+            while(current.next != null){
+                if(current.next.getData().getTeam() == 2){
+                    System.out.println(current.next.getData().getFname() + " " + current.next.getData().getLname());
+                }
+                current = current.next;
+            }
+        }
 
+    }
 }
